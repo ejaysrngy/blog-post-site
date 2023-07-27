29eb3329e67275b8c9965807276e3dae08239ef4
Ejay Sarangay <73544728+ejaysrngy@users.noreply.github.com>
2023-07-27 19:00:47 +0800

    Fix Navbar layout (#35)
    Co-authored-by: ejayxurpas <ejay.xurpas@gmail.com>

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import classes from './hero.module.scss'
+import { Poppins } from 'next/font/google'
 
 import { Card, Typography } from "@mui/material";
+
+export const poppins = Poppins({weight: "400", subsets: ['latin'], variable: '--font-poppins'})
 
 export default function HeroSection() {
 
   return (
     <div className={classes.container}>
       <Card className={classes.cardContents}>
-        <Typography className={classes.text}>
+        <Typography className={classes.title}>
           Hi! Welcome to my blog posts!
         </Typography>
-        <Typography>
+        <Typography className={classes.subtext}>
           This is a personal project for me to practice NextJS and learn the ins and outs of a Fullstack project
         </Typography>
       </Card>
