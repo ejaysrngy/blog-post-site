a431f0a7c38ef91e649c2780bc84039ebd92bfb0
Ejay Sarangay <73544728+ejaysrngy@users.noreply.github.com>
2023-08-06 14:06:57 +0800

    Style: Change Color Palette (#39)
    Co-authored-by: ejayxurpas <ejay.xurpas@gmail.com>

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,23 +1,21 @@
 import React from "react";
-import classes from './hero.module.scss'
-import { Poppins } from 'next/font/google'
+import classes from "./hero.module.scss";
+import { Poppins } from "next/font/google";
 
 import { Card, Typography } from "@mui/material";
 
-export const poppins = Poppins({weight: "400", subsets: ['latin'], variable: '--font-poppins'})
-
 export default function HeroSection() {
-
   return (
     <div className={classes.container}>
-      <Card className={classes.cardContents}>
+      <div className={classes.cardContents}>
         <Typography className={classes.title}>
           Hi! Welcome to my blog posts!
         </Typography>
         <Typography className={classes.subtext}>
-          This is a personal project for me to practice NextJS and learn the ins and outs of a Fullstack project
+          This is a personal project for me to practice NextJS and learn the ins
+          and outs of a Fullstack project
         </Typography>
-      </Card>
+      </div>
     </div>
   );
 }
